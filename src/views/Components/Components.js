ada0d604ecb7e4a6a89b294a5398a2b49c6e6347
Dan Robson <danr.236@gmail.com>
2020-01-19 10:14:23 -0500

    more repo cleanup

--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -1,38 +1,25 @@
-import React from "react";
-// nodejs library that concatenates classes
-import classNames from "classnames";
-// react components for routing our app without refresh
-import { Link } from "react-router-dom";
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
 import Tooltip from "@material-ui/core/Tooltip";
 // @material-ui/icons
-import { Apps, CloudDownload } from "@material-ui/icons";
-// core components
-import Header from "components/Header/Header.js";
+import { CloudDownload } from "@material-ui/icons";
+import styles from "assets/jss/material-kit-react/views/components.js";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+import Button from "components/CustomButtons/Button.js";
 import Footer from "components/Footer/Footer.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
-import Button from "components/CustomButtons/Button.js";
-import Parallax from "components/Parallax/Parallax.js";
+// core components
+import Header from "components/Header/Header.js";
 // sections for this page
 import HeaderLinks from "components/Header/HeaderLinks.js";
+import Parallax from "components/Parallax/Parallax.js";
+import React from "react";
 import SectionBasics from "./Sections/SectionBasics.js";
-import SectionNavbars from "./Sections/SectionNavbars.js";
-import SectionTabs from "./Sections/SectionTabs.js";
-import SectionPills from "./Sections/SectionPills.js";
-import SectionNotifications from "./Sections/SectionNotifications.js";
-import SectionTypography from "./Sections/SectionTypography.js";
-import SectionJavascript from "./Sections/SectionJavascript.js";
-import SectionCarousel from "./Sections/SectionCarousel.js";
-import SectionCompletedExamples from "./Sections/SectionCompletedExamples.js";
-import SectionLogin from "./Sections/SectionLogin.js";
-import SectionExamples from "./Sections/SectionExamples.js";
-import SectionDownload from "./Sections/SectionDownload.js";
 
-import styles from "assets/jss/material-kit-react/views/components.js";
 
 const useStyles = makeStyles(styles);
 
@@ -235,24 +222,6 @@ export default function Components(props) {
 
       <div className={classNames(classes.main, classes.mainRaised)}>
         <SectionBasics />
-        {/* <SectionNavbars />
-        <SectionTabs />
-        <SectionPills />
-        <SectionNotifications /> */}
-        <SectionTypography />
-        {/* <SectionJavascript />
-        <SectionCarousel />
-        <SectionCompletedExamples />
-        <SectionLogin />
-        <GridItem md={12} className={classes.textCenter}>
-          <Link to={"/login-page"} className={classes.link}>
-            <Button color="primary" size="lg" simple>
-              View Login Page
-            </Button>
-          </Link>
-        </GridItem>
-        <SectionExamples />
-        <SectionDownload /> */}
       </div>
       <Footer />
     </div>
